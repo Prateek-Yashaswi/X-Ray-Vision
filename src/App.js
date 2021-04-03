d78d56563045561983e8b387cc95146c75c503c8
Prateek-Yashaswi <prateekyashaswi34@gmail.com>
2021-04-03 14:41:26 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return (
+        <div id="header">
+            <p id="header-title">Rakathon 2021 <br></br> Project Name</p>
+            
+        </div>
+    );
+}
+
+export default Header;
