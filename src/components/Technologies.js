0db42d83198d155bfba325218215973aa0268c30
Rishav-Chowdhury <60723801+Rishav-Chowdhury@users.noreply.github.com>
2021-04-09 22:32:31 +0530

    Technologies Text

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -38,42 +38,41 @@ const Tech = () => {
             </div>
 
             <div className="techno">
-                <div className="t1">
+                <div className="t0">
                     <Icon icon={reactIcon} className="logo-img" />
                     <h2 className="tech-header2">React</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <p className="tech-header3">React is an open-source, 
+                    front end, JavaScript library for building user interfaces 
+                    or UI components. It is maintained by Facebook. It lets you 
+                    compose complex UIs from small and isolated pieces of code 
+                    called “components”. React is at the heart of the X-Ray Vision 
+                    Application, as the front-end of our application relies solely 
+                    on this framework, along with additional CSS libraries like Bootstrap.
                     </p>
                 </div>
 
                 <div class="t1">
                     <Icon icon={firebaseIcon} className="logo-img" />
                     <h2 className="tech-header2">Firebase</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <p className="tech-header3">Firebase is a Backend-as-a-Service - BaaS - 
+                    that started as a YC11 startup and grew into a next-generation 
+                    app-development platform on Google Cloud. It serves as a Real-Time 
+                    Database for creating mobile and web applications. It has built-in 
+                    features like analytics, authentication, databases, configuration, 
+                    file storage, and push messaging. The services are hosted in the cloud 
+                    and are easy to scale with very less effort.
                     </p>
                 </div>
 
                 <div class="t1">
                     <Icon icon={pytorchIcon} className="logo-img" />
                     <h2 className="tech-header2">PyTorch</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <p className="tech-header3">PyTorch is an open source machine learning 
+                    library based on the Torch library, used for applications such as computer 
+                    vision and natural language processing, primarily developed by Facebook's AI 
+                    Research lab. It is known for providing two of the most high-level features; 
+                    namely, tensor computations with strong GPU acceleration support and building 
+                    deep neural networks on a tape-based autograd systems.
                     </p>
                 </div>
             </div>
@@ -81,40 +80,37 @@ const Tech = () => {
             <div className="techno">
                 <div className="t2">
                     <Icon icon={nodejsIcon} className="logo-img"/>
-                    <h2 className="tech-header2">React</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <h2 className="tech-header2">Node</h2>
+                    <p className="tech-header3">Node.js is an open-source, cross-platform, 
+                    back-end JavaScript runtime environment that runs on the V8 engine and 
+                    executes JavaScript code outside a web browser. Node.js lets developers 
+                    use JavaScript to write command line tools and for server-side scripting 
+                    to produce dynamic web page content before the page is sent to the user's 
+                    web browser. It used to write server-side applications with access to the 
+                    operating system.
                     </p>
                 </div>
 
                 <div class="t2">
                     <Icon icon={herokuIcon} className="logo-img"/>
-                    <h2 className="tech-header2">Firebase</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <h2 className="tech-header2">Heroku</h2>
+                    <p className="tech-header3">Heroku is a container-based cloud Platform as 
+                    a Service (PaaS) that enables developers to build, run, and operate applications 
+                    entirely in the cloud. It is a cloud platform as a service supporting several programming 
+                    languages. It has integrated data services and a powerful ecosystem for deploying and 
+                    running modern applications. Polymorphism and Scalability are reasons why Heroku is 
+                    a preferred platform.
                     </p>
                 </div>
 
-                <div class="t2">
+                <div class="t3">
                     <img src="https://miro.medium.com/max/4000/0*VbMjGBHMC6GnDKUp.png" className='logo-img'/>
-                    <h2 className="tech-header2">PyTorch</h2>
-                    <p className="tech-header3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras ut nibh non leo faucibus gravida.
-                    Nulla volutpat lobortis erat commodo mollis.
-                    Donec ornare posuere eleifend. Sed vel blandit nunc.
-                    Sed lobortis lacinia massa nec rutrum.
-                    Curabitur diam tellus, eleifend sed tellus non, pharetra faucibus nunc.
-                    Nam nisl urna, pulvinar eu semper vitae, semper efficitur velit. Suspendisse potenti.
+                    <h2 className="tech-header2">Detectron</h2>
+                    <p className="tech-header3">Detectron2 is Facebook AI Research's next generation software 
+                    system that implements state-of-the-art object detection algorithms. It is a ground-up 
+                    rewrite of the previous version, Detectron, and it originates from maskrcnn-benchmark. 
+                    It is powered by the PyTorch deep learning framework. Models can be exported to TorchScript 
+                    format or Caffe2 format for deployment. Our Model utilises this library to localize abnormalities.
                     </p>
                 </div>
             </div>
